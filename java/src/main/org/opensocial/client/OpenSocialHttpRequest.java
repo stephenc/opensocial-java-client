d1ba56966eab93ef01199374d9bc7f1c664b9d1e
Jason Cooper <apijason@google.com>
2009-02-27 22:20:44 +0000

    Rolled back changes to OpenSocialHttpRequest class to remove dependency on Apache HttpClient library

--- java/src/main/org/opensocial/client/OpenSocialHttpRequest.java
+++ java/src/main/org/opensocial/client/OpenSocialHttpRequest.java
@@ -16,22 +16,13 @@
 
 package org.opensocial.client;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * An internal object which represents a single HTTP request and contains a
@@ -43,80 +34,142 @@ import java.io.InputStreamReader;
  */
 public class OpenSocialHttpRequest {
 
+  private String method;
+  private String body;
   private OpenSocialUrl url;
-  private HttpRequestBase request;
-  
-  public OpenSocialHttpRequest(String method, OpenSocialUrl url) {
-    this.url = url;
+  private HttpURLConnection connection;
 
-    if (method.equals("GET")) {
-      this.request = new HttpGet();
-    } else if (method.equals("PUT")) {
-      this.request = new HttpPut();
-    } else if (method.equals("POST")) {
-      this.request = new HttpPost();
-    } else if (method.equals("DELETE")) {
-      this.request = new HttpDelete();
+  public OpenSocialHttpRequest(String method, OpenSocialUrl url) throws IOException {
+    this.method = method;
+    this.body = null;
+    this.connection = null;    
+    this.setUrl(url);
+  }
+
+  /**
+   * Initializes connection if necessary before establishing the connection,
+   * including writing POST data to the connection's output stream if
+   * applicable.
+   * 
+   * @return HTTP status code of request, e.g. 200, 401, 500, etc.
+   * @throws IOException
+   */
+  public String execute() throws IOException {
+    if (this.connection == null) {
+      this.connection = this.getConnection();
     }
-  }
 
-  public String execute() throws OpenSocialRequestException, IOException {
-    HttpClient httpClient = new DefaultHttpClient();
-    httpClient.getParams().setBooleanParameter( "http.protocol.expect-continue", false );
-    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+    this.connection.setRequestMethod(this.method);
 
-    try {
-      this.request.setURI(this.url.toUri());
-      return httpClient.execute(this.request, responseHandler);
-    } catch (java.net.URISyntaxException e) {
-      throw new OpenSocialRequestException("Malformed URL " + this.url.toString());
-    }    
-  }
-
-  public void setBody(String body) {
-    String requestMethod = this.request.getMethod();
-    
-    if (requestMethod.equals("PUT") || requestMethod.equals("POST")) {
-      HttpEntityEnclosingRequestBase entityRequest =
-        (HttpEntityEnclosingRequestBase) this.request;
-
-      try {
-        StringEntity requestEntity = new StringEntity(body, "UTF-8");
-        requestEntity.setContentType("application/x-www-form-urlencoded");
-        entityRequest.setEntity(requestEntity);        
-      } catch (java.io.UnsupportedEncodingException e) {
-      }
-    }
-  }
-
-  public String getBody() throws IOException {
-    String requestMethod = this.request.getMethod();
-
-    if (requestMethod.equals("PUT") || requestMethod.equals("POST")) {
-      HttpEntityEnclosingRequestBase entityRequest =
-        (HttpEntityEnclosingRequestBase) this.request;
-      HttpEntity bodyEntity = entityRequest.getEntity();
-    
-      StringBuilder sb = new StringBuilder();
-      BufferedReader reader = new BufferedReader(
-        new InputStreamReader(bodyEntity.getContent()));
-
-      String line = null;
-      while ((line = reader.readLine()) != null) {
-        sb.append(line);
-      }
-
-      return sb.toString();
+    if (this.body == null) {
+      this.connection.connect();
     } else {
-      return null;
+      this.connection.setDoOutput(true);
+
+      OutputStreamWriter wr = new OutputStreamWriter(
+          this.connection.getOutputStream());
+      wr.write(this.body);
+      wr.flush();
+      wr.close();
     }
+
+    return this.getResponseString();
   }
 
+  /**
+   * After executing the request, returns the server response as an InputStream
+   * object.
+   * 
+   * @throws IOException
+   */
+  public InputStream getResponseStream() throws IOException {
+    return this.connection.getInputStream();
+  }
+
+  /**
+   * After executing the request, transforms response output contained in the
+   * connection's InputStream object into a string representation which can
+   * later be parsed into a more meaningful object, e.g. OpenSocialObject. 
+   *
+   * @throws IOException if the open connection's input stream is not
+   *         retrievable or accessible
+   */
+  public String getResponseString() throws IOException {
+    InputStream responseStream = this.getResponseStream();
+
+    StringBuilder sb = new StringBuilder();
+    BufferedReader reader = new BufferedReader(
+        new InputStreamReader(responseStream));
+
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      sb.append(line);
+    }
+
+    responseStream.close();
+
+    return sb.toString();
+  }
+
+  /**
+   * Returns instance variable: method.
+   */
+  public String getMethod() {
+    return this.method;
+  }
+
+  /**
+   * Returns instance variable: body.
+   */
+  public String getBody() {
+    return this.body;
+  }
+
+  /**
+   * Returns instance variable: url.
+   */
   public OpenSocialUrl getUrl() {
     return this.url;
   }
 
-  public String getMethod() {
-    return this.request.getMethod();
+  /**
+   * Sets instance variable method to passed String.
+   */
+  public void setMethod(String method) {
+    this.method = method;
+  }
+
+  /**
+   * Sets instance variable body to passed String and automatically sets
+   * request method to POST.
+   */
+  public void setBody(String body) {
+    this.body = body;
+  }
+
+  /**
+   * Sets instance variable url to passed OpenSocialUrl object.
+   * 
+   * @param requestUrl
+   * @throws IOException
+   */
+  private void setUrl(OpenSocialUrl requestUrl) throws IOException {
+    this.url = requestUrl;
+  }
+
+  /**
+   * Opens a new HTTP connection for the URL associated with this object.
+   * 
+   * @return Opened connection
+   * @throws IOException if URL protocol is not http
+   */
+  private HttpURLConnection getConnection() throws IOException {
+    URL url = new URL(this.url.toString());
+
+    if (!url.getProtocol().startsWith("http")) {
+      throw new IOException("Unsupported scheme:" + url.getProtocol());
+    }
+
+    return ((HttpURLConnection) url.openConnection());
   }
 }
